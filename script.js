671538e46904b22f2da100bc6c1319ea8ece38ed
Andrea Barbuto <barbutoandrea99@gmail.com>
2025-12-10 18:20:43 +0100

    feat: added button with js-script to turn lamp on click

--- script.js
+++ script.js
@@ -3,5 +3,7 @@ const button = document.getElementById("js-button");
 
 button.addEventListener("click" , function() {
 
-    
+    let imageJs = document.getElementById("lamp-image");
+    imageJs.src = "./img/yellow_lamp.png";
+    imageJs.alt = "lamp-on";
 })
