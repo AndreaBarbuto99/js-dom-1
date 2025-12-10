0c36d6cce403d874314e7289e1df49515324ff5f
Andrea Barbuto <barbutoandrea99@gmail.com>
2025-12-10 18:55:40 +0100

    feat: added bonus with button change

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const button = document.getElementById("js-button");
+let imageJs = document.getElementById("lamp-image");
 
 
 button.addEventListener("click" , function() {
-
-    let imageJs = document.getElementById("lamp-image");
-    imageJs.src = "./img/yellow_lamp.png";
-    imageJs.alt = "lamp-on";
+    if (button.innerHTML.includes("Accendi"))
+    {
+        imageJs.src = "./img/yellow_lamp.png";
+        imageJs.alt = "lamp-on";
+        button.innerHTML = "Spegni";
+        
+    }
+    else 
+    {
+        imageJs.src = "./img/white_lamp.png";
+        imageJs.alt = "lamp-off";
+        button.innerHTML = "Accendi";
+       
+    }
 })
+
